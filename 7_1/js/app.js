08f9ef3a963041972b8737ddedf0f9e9aecaae0d
wpiusa <wpiusa@gmail.com>
2017-03-10 09:27:15 -0800

    Add files via upload

---
+++ 7_1/js/app.js
@@ -0,0 +1,11 @@
+
+function calculateArea(radious){
+  var pi=3.14;
+  var areaCircle=pi*radious*radious;
+  return areaCircle ;
+}
+
+var areaOfCirle=calculateArea(4);
+//when r = 7 ; area = 154
+console.log("when r=4");
+console.log("area= "+areaOfCirle);
